1c5f91bb56e516bd3e7d676971961096d1d30ee4
Krzysiek Przystarz <frodson35@gmail.com>
2023-01-22 12:57:15 +0100

    Zadanie 10.1 - sukces, wszystko działa :D Dziękuję za świetne wytłumaczenie Sławek! Pozdrawiam chłopaki ;)

--- src/main/java/pl/javastart/task/BooksArray.java
+++ src/main/java/pl/javastart/task/BooksArray.java
@@ -2,41 +2,46 @@ package pl.javastart.task;
 
 import java.util.Scanner;
 
-public class BooksArray extends Book {
+public class BooksArray {
     private static final int MAX_ARRAY_SIZE = 3;
     Book[] books = new Book[MAX_ARRAY_SIZE];
 
-    public BooksArray(String title, int pages) {
-        super(title, pages);
+    public void addUniqueBook() {
+        System.out.println("Podaj informację o trzech unikalnych książkach");
+        for (int i = 0; i < books.length;) {
+            Book book = createBook();
+            if (isDuplicate(book, books)) {
+                System.out.println("Duplikat");
+            } else {
+                books[i] = book;
+                i++;
+            }
+        }
     }
 
     public Book createBook() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Podaj Tytuł");
-        setTitle(scanner.nextLine());
+        String title = scanner.nextLine();
         System.out.println("Podaj ilość stron");
-        setPages(scanner.nextInt());
+        int pages = scanner.nextInt();
         scanner.nextLine();
-        return new Book(getTitle(), getPages());
-    }
-
-    public void foundAndAddUniqueBook() {
-        System.out.println("Podaj informację o trzech unikalnych książkach");
-        for (int i = 0; i < books.length; i++) {
-            books[i] = createBook();
-            Book book = books[i];
-            if (books[i].equals(book)) {
-                System.out.println("Dupilkat");
-            } else {
-                books[i] = new Book(getTitle(), getPages());
-            }
-        }
+        return new Book(title, pages);
     }
 
     public void getInfoAboutUniqueBooks() {
         System.out.println(books[0].toString());
         System.out.println(books[1].toString());
         System.out.println(books[2].toString());
+    }
+
+    public boolean isDuplicate(Book book, Book[] books) {
+        for (Book book1 : books) {
+            if (book.equals(book1)) {
+                return true;
+            }
+        }
+        return false;
     }
 
     @Override
--- src/main/java/pl/javastart/task/Main.java
+++ src/main/java/pl/javastart/task/Main.java
@@ -3,9 +3,8 @@ package pl.javastart.task;
 public class Main {
 
     public static void main(String[] args) {
-        BooksArray booksArray = new BooksArray("a", 1);
-        booksArray.foundAndAddUniqueBook();
-//        booksArray.createBook();
+        BooksArray booksArray = new BooksArray();
+        booksArray.addUniqueBook();
         booksArray.getInfoAboutUniqueBooks();
     }
 }
